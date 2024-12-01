4546ebbb535d10594bccb872e47cc2ae7c4f24e2
matiasbiagetti <biagettimati@gmail.com>
2024-12-01 17:31:04 -0300

    typo

--- src/pages/verify-email.jsx
+++ src/pages/verify-email.jsx
@@ -1,4 +1,4 @@
-emaimport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
